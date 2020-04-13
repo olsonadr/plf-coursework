d4639ff3cca195dfaa7ca42709e89e3ac22c58bc
Nick Olson <olsonnick10@yahoo.com>
2020-04-13 22:53:28 +0000

    Skeleton code for shape implementation

--- haskell/main.hs
+++ haskell/main.hs
@@ -2,35 +2,79 @@ main :: IO()
 main = print ("File loaded!")
 
 
--- Import recommended utilities --
-import HW1types
-
-
 -- Creating necessary types --
-type Node   = Int
-type Edge   = (Node,Node)
-type Graph  = [Edge]
-type Path   = [Node]
+type Number = Int
+
+type Point  = (Number,Number)
+type Length = Number
+
+data Shape  = Pt Point                  -- center
+            | Circle Point Length       -- center radius
+            | Rect Point Length Length  -- lower-left, width, height
+            deriving Show
+            
+type Figure = [Shape]
+
+type BBox   = (Point,Point) -- (lower-left, upper-right)
 
 
--- nodes function: computes list of nodes in a graph --
-nodes :: Graph -> [Node]
-nodes [] = [] -- placeholder --
+-- width function: computes the width of a shape --
+width :: Shape -> Length
+width (Pt _)        = 0
+width (Circle _ r)  = r+r
+width (Rect _ w _)  = w -- maybe done? --
 
 
--- suc function: computes list of successors of a node in a graph --
-suc :: Node -> Graph -> [Node]
-suc _ [] = []
-suc x gr = [] -- placeholder --
+-- bbox function: computes the bounding box of the shape --
+bbox :: Shape -> BBox
+bbox (Pt p) = (p, p) -- incomplete --
 
 
--- detach function: removes a node and its edges from a graph --
-detach :: Node -> Graph -> Graph
-detach _ [] = []
-detach x gr = gr -- placeholder --
+-- minX function: computes the minimum x coordinate of the shape --
+minX :: Shape -> Number
+minX (Pt (x,y)) = x -- incomplete --
 
 
--- cyc function: creates a cycle for a given number of nodes --
-cyc :: Int -> Graph
-cyc 0 = []
-cyc n = [] -- placeholder --
+-- addPt function: adds the components of two points --
+addPt :: Point -> Point -> Point
+addPt (x1,y1) (x2,y2) = (x1+x2, y1+y2) -- maybe done? --
+
+
+-- move function: translates a shape's position by a given vector --
+move :: Shape -> Point -> Shape
+move (Pt p) d = Pt (addPt p d) -- incomplete --
+
+
+-- moveToX function: translates a shape's x position to a value --
+moveToX :: Shape -> Number -> Shape
+moveToX (Pt (_,y)) x = Pt (x,y) -- incomplete --
+
+
+-- moveAllToX function: translate all shapes in a figure to a x position --
+moveAllToX :: Figure -> Number -> Figure
+moveAllToX [] _         = []
+moveAllToX (sh:[]) x    = [(moveToX sh x)]
+moveAllToX (sh:shs) x   = [(moveToX sh x)] ++ (moveAllToX shs x) --maybe complete?--
+
+-- minNum function: finds the smaller of two numbers --
+minNum :: Number -> Number -> Number
+minNum x1 x2    | x1 <= x2  = x1
+                | otherwise = x2 -- maybe complete? --
+
+
+-- minMinX function: finds smallest minX in a figure --
+minMinX :: Figure -> Number
+minMinX []          = 0
+minMinX (sh:[])     = minX sh
+minMinX (sh:shs)    = minNum (minX sh) (minMinX shs) -- maybe complete? --
+
+
+-- alignLeft function: moves all shapes in a figure to have the same minX value --
+alignLeft :: Figure -> Figure
+alignLeft []    = []
+alignLeft fig   = moveAllToX fig (minMinX fig) -- maybe complete? --
+
+
+-- inside function: computes if one shape is contained within another --
+--inside :: Shape -> Shape -> Bool
+-- for this one maybe we check if the bounding box is contained withing the other, unless its a point in a point which takes special consideration because equal points
