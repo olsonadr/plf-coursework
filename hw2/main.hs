730cc5faf4e0c9b107e3bb6ab92a9118d793ef72
Nick Olson <olsonnick10@yahoo.com>
2020-04-22 03:31:07 +0000

    Pretty showing done

--- hw2/main.hs
+++ hw2/main.hs
@@ -7,29 +7,56 @@ module HW2 where
 
 -- == For MiniLogo == --
 
--- cmd      ::= pen mode
---           |  moveto (pos,pos)
---           |  def name ( pars ) cmd
---           |  call name ( vals )
---           |  cmd; cmd
+-- cmd      ::= pen 'mode'
+--           |  moveto ('pos','pos')
+--           |  def 'name' ( 'pars' ) 'cmd'
+--           |  call 'name' ( 'vals' )
+--           |  'cmd'; 'cmd'
 -- mode     ::= up | down
--- pos      ::= num | name
--- pars     ::= name, pars | name
--- vals     ::= num, vals | num
+-- pos      ::= 'num' | 'name'
+-- pars     ::= 'name', 'pars' | 'name'
+-- vals     ::= 'num', 'vals' | 'num'
 
 data Cmd    = Pen     Mode                  -- change pen mode (up or down)
-            | MoveTo  Pos                   -- move pen to given position
-            | Def     String [String] [Cmd] -- define macro with name, parameters, and body
-            | Call    String [Integer]      -- call macro with given name, passing in values
-            deriving Show
-data Mode   = Up | Down                     -- allowed pen modes
-            deriving Show
-data Pos    = N (Integer, Integer)          -- allowing numerical positions
-            | P (String, String)            -- allowing parameter names for positions
+            | MoveTo  (Pos, Pos)            -- move pen to given position
+            | Def     String Params Cmd     -- define macro with name, parameters, and body
+            | Call    String Args           -- call macro with given name, passing in values
+            | SeqC Cmd Cmd                  -- allow chaining of commands
 
-instance Show Pos where  
-    show (N (n1, n2)) = show (n1, n2)  
-    show (P (s1, s2)) = show (s1, s2)
+data Mode   = Up | Down                     -- allowed pen modes
+
+data Pos    = Num Integer                   -- allowing numerical positions      
+            | Var String                    -- allowing variable names
+
+data Params = SeqP String Params            -- allowing chaining of multiple params
+            | OneP String                   -- enforcing at least one (not a list)
+
+data Args   = SeqA Integer Args             -- allowing chaining of multiple args
+            | OneA Integer                  -- enforcing at least one (not a list)
+
+
+instance Show Params where  
+    show (SeqP s ps)    = s ++ "," ++ show ps
+    show (OneP s)       = s
+
+instance Show Args where  
+    show (SeqA i as)    = show i ++ "," ++ show as
+    show (OneA i)       = show i
+
+instance Show Pos where
+    show (Num i)        = show i
+    show (Var s)        = s
+
+instance Show Cmd where
+    show (Pen m)        = "pen " ++ show m ++ ";"
+    show (MoveTo (a,b)) = "moveto (" ++ show a ++ "," ++ show b ++ ");"
+    show (Def n p c)    = "def " ++ show n ++ " ( " ++ show p ++ " ) " ++ show c
+    show (Call n a)     = "call " ++ show n ++ "( " ++ show a ++ " ); "
+    show (SeqC c1 c2)   = show c1 ++ " " ++ show c2
+
+instance Show Mode where
+    show Up     = "up"
+    show Down   = "down"
 
 
 
@@ -43,7 +70,10 @@ instance Show Pos where
 -- in MiniLogo:
 --     def vector ( x1, y1, x2, y2 ) pen up; moveto (x1,y1); pen down; moveto (x2,y2); pen up
 -- in Abstract Syntax (haskell data types):
-vector = Def "vector" ["x1", "y1", "x2", "y2"] [Pen Up, MoveTo (P ("x1", "y1")), Pen Down, MoveTo (P ("x2", "y2")), Pen Up]
+v_name = "vector"
+v_par = (SeqP "x1" (SeqP "y1" (SeqP "x2" (OneP "y2")))) :: Params
+v_cmd = (SeqC (Pen Up) (SeqC (MoveTo (Var "x1", Var "y1")) (SeqC (Pen Down) (SeqC (MoveTo (Var "x2", Var "y2")) (Pen Up))))) :: Cmd
+vector = (Def v_name v_par v_cmd) :: Cmd
 
 
 
@@ -63,7 +93,7 @@ steps _ = Pen Up; -- placeholder --
 -- ========== Helper Functions ========== --
 -- ====================================== --
 
--- Main function (help)
+-- Main function (on load)
 main = do
     putStrLn "HW2 Loading! Available helper functions:"
     --putStrLn " ~= bg_test  :  test Bag functions"
