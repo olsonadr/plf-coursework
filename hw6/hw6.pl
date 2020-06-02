83d857e40c772636127f0e8936e825625b2956df
Nick Olson <olsonnick10@yahoo.com>
2020-06-02 03:53:28 +0000

    hw6 completed, index.py updated to print hw6.pl to terminal

--- hw6/hw6.pl
+++ hw6/hw6.pl
@@ -19,40 +19,86 @@ enroll(mary,261).
 enroll(john,381).
 enroll(jim,399).
 
-/* Part (a) - schedule[3] gives classrooms and times of their classes (student, location, time) */
-schedule(Name,Place,Time) :- enroll(Name,Class), where(Class,Place), when(Class,Time).
+/* Part (a) -   schedule[3] gives classrooms and times of their
+ *              classes (student, location, time)
+ */
+schedule(Name,Place,Time) 	:- 	enroll(Name,Class),
+								where(Class,Place),
+								when(Class,Time).
 
-/* Part (b) - usage[2] gives all times a classroom is used (location, time) */
-usage(Place,Time) :- where(Class,Place), when(Class,Time).
+/* Part (b) -   usage[2] gives all times a classroom is used
+ *              (location, time)
+ */
+usage(Place,Time) 	:- 	where(Class,Place),
+						when(Class,Time).
 
-/* Part (c) - conflict[2] computes if two classes are assigned to same place at same time (Class, Class) */
-conflict(Class1,Class2) :- where(Class1,P), where(Class2,P), when(Class1,T), when(Class2,T), Class1 \= Class2.
+/* Part (c) -   conflict[2] computes if two classes are assigned
+ *              to same place at same time (Class, Class)
+ */
+conflict(Class1,Class2) :- 	where(Class1,P),
+							where(Class2,P),
+							when(Class1,T),
+							when(Class2,T),
+							Class1 \= Class2.
 
-/* Part (d) - meet[2] checks if pair of students have class in same room at same time or have back-to-back classes, ignoring conflicting classes (name1, name2) */
-meet(Student1,Student2) :- schedule(Student1,P,T), schedule(Student2,P,T), Student1 \= Student2.
-meet(Student1,Student2) :- schedule(Student1,P,T1), schedule(Student2,P,T2), T1 =:= T2-1, Student1 \= Student2.
+/* Part (d) -   meet[2] checks if pair of students have class
+ * 				in same room at same time or have back-to-back
+ * 				classes, ignoring conflicting classes
+ * 				(name1, name2)
+ */
+meet(Student1,Student2) :- 	schedule(Student1,P,T),
+							schedule(Student2,P,T),
+							Student1 \= Student2.
+meet(Student1,Student2) :- 	schedule(Student1,P,T1),
+							schedule(Student2,P,T2),
+							T1 =:= T2-1,
+							Student1 \= Student2.
 
 
 /*** Exercise 2 ***/
 
-/* Part (a) - rdup[2] removes duplicates from an ordered list L, binding result to M (L,M) */
-rdup(L,M) :- L=[], M=[].
-rdup(L,M) :- append(RemainingL, [LastL], L), rdup(RemainingL, RecursRes), member(LastL, RecursRes), M=RecursRes.
-rdup(L,M) :- append(RemainingL, [LastL], L), rdup(RemainingL, RecursRes), not(member(LastL, RecursRes)), append(RecursRes, [LastL], M).
+/* Part (a) - 	rdup[2] removes duplicates from an ordered list
+ * 				L, binding result to M (L,M)
+ */
+rdup(L,M) 	:- 	L=[], M=[].
+rdup(L,M) 	:- 	append(RemainingL, [LastL], L),
+				rdup(RemainingL, RecursRes),
+				member(LastL, RecursRes),
+				M=RecursRes.
+rdup(L,M) 	:- 	append(RemainingL, [LastL], L),
+				rdup(RemainingL, RecursRes),
+				not(member(LastL, RecursRes)),
+				append(RecursRes, [LastL], M).
 
-/* Part (b) - flat[2] binds to F the flat list of all elements in L (L,F) */
-flat(L,F) :- F=L. /*dummy*/
+/* Part (b) - 	flat[2] binds to F the flat list of all
+ * 				elements in L (L,F)
+ */
 /* flat([1,[1,2],[2]], [1,1,2,2]). => true */
+flat([],[]).
+flat([L|LS],F) :- 	is_list(L),
+					flat(L, FlatL),
+					flat(LS, RecursRes),
+					append(FlatL, RecursRes, F).
+flat([L|LS],F) :- 	not(is_list(L)),
+					flat(LS, RecursRes),
+					append([L], RecursRes, F).
 
-/* Part (c) - project[3] selects elements from list L by positions in list I and collects them in a result list R (I,L,R) */
-subeach(I,R) :- (I=[], R=[]) ; (append([FirstI],RestI,I), append([FirstR],RestR,R), FirstR is (FirstI-1), subeach(RestI,RestR)).
-haszero([0|_]).
-haszero([_|IS]) :- haszero(IS).
-project(_,[],[]).
-project(I,[_|LS],R) :- not(haszero(I)), subeach(I,NextI), project(NextI,LS,R).
-project(I,[L|LS],R) :- haszero(I), subeach(I,NextI), project(NextI,LS,PrevR), append([L],PrevR,R).
-
-/*
+/* Part (c) - 	project[3] selects elements from list L by
+ * 				positions in list I and collects them in a
+ * 				result list R (I,L,R) O(n)
+ */
+project(I,L,R) :- projectreal(I,L,R,0).
+projectreal(_,[],[],_).
+projectreal([],_,[],_).
+projectreal([I|IS],[_|LS],R,J) :-	I =\= J,
+									NewJ is (J+1),
+									projectreal([I|IS],LS,R,NewJ).
+projectreal([I|IS],[L|LS],R,J) :-	I =:= J,
+									NewJ is (J+1),
+									projectreal(IS,LS,PrevR,NewJ),
+									append([L], PrevR, R).
+								
+/* project example (I,L,R)
 [0,2,3], 	[1,2,3,4,5,6], 	[1,3,4]
 [-1,1,2], 	[2,3,4,5,6], 	[3,4]
 [-2,0,1], 	[3,4,5,6], 		[3,4]
@@ -61,3 +107,5 @@ project(I,[L|LS],R) :- haszero(I), subeach(I,NextI), project(NextI,LS,PrevR), ap
 [-5,-3,-2], [6], 			[]
 [-6,-4,-3], [], 			[]
 */
+
+
--- index.py
+++ index.py
@@ -8,11 +8,12 @@ print("    (2) HW2 - Abstract Syntax")
 print("    (3) HW3 - Semantics")
 print("    (4) HW4 - Types")
 print("    (5) HW5 - Blocks")
+print("    (6) HW6 - Prolog")
 print("    (-) exit\n")
 
 
 def isValid(choice):
-    good_choices = ["1", "load1", "2", "load2", "3", "load3", "4", "load4", "5", "load5", "exit", "-"]
+    good_choices = ["1", "load1", "2", "load2", "3", "load3", "4", "load4", "5", "load5", "6", "load6", "exit", "-"]
     for good in good_choices:
         if choice == good:
             return True
@@ -40,6 +41,13 @@ if user_choice and user_choice != "exit" and user_choice != "-":
 
     elif user_choice[-1] == '5':
         os.system("cd hw5; runhaskell main.hs; ghci main.hs")
+
+    elif user_choice[-1] == '6':
+        print("Homework 6 Prolog File: {")
+        with open("hw6/hw6.pl") as fp:
+            for cnt, line in enumerate(fp):
+                print("\t{0}  {1}".format(cnt, line), end=' ')
+        print("}")
 
     else:
         print("Module not found...")
